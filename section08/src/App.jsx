082ae851d126be9a9cf5a6137e0e62700249f11a
DT231006\user <happyddo42@gmail.com>
2025-07-03 00:17:07 +0900

    feat: ToDo List 구현하기

---
+++ section08/src/App.jsx
@@ -0,0 +1,42 @@
+import "./App.css";
+import { useState, useRef } from "react";
+import Header from "./components/Header";
+import TodoEditor from "./components/TodoEditor";
+import TodoList from "./components/TodoList";
+
+function App() {
+  const [todo, setTodo] = useState([]);
+  const idRef = useRef(3);
+
+  const onCreate = (content) => {
+    const newItem = {
+      id: idRef.current,
+      content,
+      isDone: false,
+      createdDate: new Date().getTime(),
+    };
+    setTodo([newItem, ...todo]);
+    idRef.current += 1;
+  };
+
+  const onUpdate = (targetId) => {
+    setTodo(
+      todo.map((it) =>
+        it.id === targetId ? { ...it, isDone: !it.isDone } : it
+      )
+    );
+  };
+  const onDelete = (targetId) => {
+    setTodo(todo.filter((it) => it.id !== targetId));
+  };
+
+  return (
+    <div className="App">
+      <Header />
+      <TodoEditor onCreate={onCreate} />
+      <TodoList todo={todo} onUpdate={onUpdate} onDelete={onDelete} />
+    </div>
+  );
+}
+
+export default App;
