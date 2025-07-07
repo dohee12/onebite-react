7af9315bd93a3017d5eed5fc6b9214c76366e37c
DT231006\user <happyddo42@gmail.com>
2025-07-07 15:10:52 +0900

    feat: 최적화

---
+++ section10/src/App.jsx
@@ -0,0 +1,65 @@
+import "./App.css";
+import { useCallback, useReducer, useRef } from "react";
+import Header from "./components/Header";
+import TodoEditor from "./components/TodoEditor";
+import TodoList from "./components/TodoList";
+import Exam from "./components/Exam";
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "CREATE": {
+      return [action.newItem, ...state];
+    }
+    case "UPDATE": {
+      return state.map((it) =>
+        it.id === action.targetId ? { ...it, isDone: !it.isDone } : it
+      );
+    }
+    case "DELETE": {
+      return state.filter((it) => it.id !== action.targetId);
+    }
+    default:
+      return state;
+  }
+}
+
+function App() {
+  const [todo, dispatch] = useReducer(reducer, []);
+  const idRef = useRef();
+
+  const onCreate = useCallback((content) => {
+    dispatch({
+      type: "CREATE",
+      newItem: {
+        id: idRef.current++,
+        content,
+        isDone: false,
+        createdDate: new Date().getTime(),
+      },
+    });
+  }, []);
+
+  const onUpdate = useCallback((targetId) => {
+    dispatch({
+      type: "UPDATE",
+      targetId,
+    });
+  }, []);
+
+  const onDelete = useCallback((targetId) => {
+    dispatch({
+      type: "DELETE",
+      targetId,
+    });
+  }, []);
+
+  return (
+    <div className="App">
+      <Header />
+      <TodoEditor onCreate={onCreate} />
+      <TodoList todo={todo} onUpdate={onUpdate} onDelete={onDelete} />
+    </div>
+  );
+}
+
+export default App;
---
+++ section10/src/components/TodoList.jsx
@@ -0,0 +1,60 @@
+import "./TodoList.css";
+import TodoItem from "./TodoItem";
+import { useMemo, useState } from "react";
+
+const TodoList = ({ todo, onUpdate, onDelete }) => {
+  const [search, setSearch] = useState("");
+  const onChangeSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const getSearchResult = () => {
+    return search === ""
+      ? todo
+      : todo.filter((it) =>
+          it.content.toLowerCase().includes(search.toLowerCase())
+        );
+  };
+
+  const analyzeTodo = useMemo(() => {
+    const totalCount = todo.length;
+    const doneCount = todo.filter((it) => it.isDone).length;
+    const notDoneCount = totalCount - doneCount;
+    return {
+      totalCount,
+      doneCount,
+      notDoneCount,
+    };
+  }, [todo]);
+
+  const { totalCount, doneCount, notDoneCount } = analyzeTodo;
+
+  return (
+    <div className="TodoList">
+      <h4>Todo List🌿</h4>
+      <div>
+        <div>총개수: {totalCount}</div>
+        <div>완료된 일: {doneCount}</div>
+        <div>아직 완료하지 못한 일: {notDoneCount}</div>
+      </div>
+      <input
+        value={search}
+        onChange={onChangeSearch}
+        className="searchbar"
+        placeholder="검색어를 입력하세요"
+      />
+      <div className="list_wrapper">
+        {getSearchResult().map((it) => (
+          <TodoItem
+            key={it.id}
+            {...it}
+            onUpdate={onUpdate}
+            onDelete={onDelete}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default TodoList;
